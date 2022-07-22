5d69c756845ad24fd3bb8d3038970d4b177efbfb
bhavya-tech <bhavya.p@ahduni.edu.in>
2022-07-22 16:28:55 +0530

    frontend modifications

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 import { useColorMode } from '@docusaurus/theme-common';
@@ -27,13 +27,15 @@ const FeatureList = [
     Svg: require('@site/static/img/production-ready.svg').default,
     description: (
       <>
-        Repetitive tasks are automated so the server can run with minimal monitoring.      
+        Repetitive tasks are automated so the server can run with minimal monitoring.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+const MyLogo = require('@site/static/img/logo.svg').default;
+
+function Feature({ Svg, title, description }) {
   var [svgColor, setSvgColor] = useState(0);
   const { colorMode } = useColorMode();
 
@@ -43,14 +45,14 @@ function Feature({Svg, title, description}) {
     );
   }, [colorMode]);
 
-  if(colorMode == undefined){
+  if (colorMode == undefined) {
     return <>Loading...</>;
   }
 
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center padding-vert--md">
-        <Svg className={styles.featureSvg} role="img" fill={svgColor} test={svgColor} />
+        <Svg className={styles.featureSvg} role="img" fill={svgColor} />
       </div>
       <div className="text--center padding-horiz--md padding-vert--md">
         <h3>{title}</h3>
@@ -60,14 +62,42 @@ function Feature({Svg, title, description}) {
   );
 }
 
+function Logo() {
+  return (
+    <div className={clsx('col col--12')}>
+      <div className="text--center padding-vert--md">
+
+        <div className="text--center " >
+          <a href='https://github.com/bhavya-tech' target="_blank" >
+            <MyLogo width={"15%"} />
+          </a>
+        </div>
+        <div className="text--center" >
+          Curated by bhavya-tech
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
+    <section className={styles.features} style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <div className='column' >
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+
+        <div className="row">
+          <div className='container'>
+            <Logo />
+          </div>
         </div>
       </div>
     </section>
